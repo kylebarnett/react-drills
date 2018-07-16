23ffb4a5d21a143cc18015f35dcff34c4ed4eac3
Kyle Barnett <kbarnett23@gmail.com>
2018-07-16 16:42:19 -0600

    Finished app-6 project

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state={
+      list: [],
+      input: ''
+    }
+
+    this.handleAddTask = this.handleAddTask.bind(this)
+  }
+
+  handleChange(val){
+    this.setState({
+      input: val
+    })
+  }
+
+  handleAddTask(){
+    this.setState({
+      list: [...this.state.list, this.state.input],
+      input: ''
+    })
+  }
+
   render() {
+    let list = this.state.list.map( (element, index) => {
+      return (
+        <Todo key={index} task={element} />
+      )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Get 'er Doneeeee! </h1>
+          <div>
+          <input value={this.state.input}
+            placeholder="Enter Here"
+            onChange={ (e) => this.handleChange(e.target.value)}>
+          </input>
+        <button onClick={this.handleAddTask}>Add</button>
+          </div>
+        < br />
+        {list}
       </div>
     );
   }
